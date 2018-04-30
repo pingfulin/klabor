4f9549cf807514ad32b3bc0dba2643b349fd65e0
pingfulin <36759454+pingfulin@users.noreply.github.com>
2018-04-30 17:02:36 +0800

    10704301650
    10704301650

--- js/test_js.js
+++ js/test_js.js
@@ -291,3 +291,29 @@ data : {
 });
 }
 }
+
+function geolocate() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+              center: geolocation,
+              radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+          });
+        }
+      function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('tw_adr')),
+            {types: ['geocode']});
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        
+      }
