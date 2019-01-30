5eeadd93044610e7ae14c1b453f42dd9f0d65d47
pingfulin <36759454+pingfulin@users.noreply.github.com>
2019-01-30 11:52:34 +0800

    Update test_js.js

--- js/test_js.js
+++ js/test_js.js
@@ -210,7 +210,7 @@
 
    $.ajax({
  
- url : "https://api-ssl.bitly.com/v3/shorten?format=txt&login=o_2p0k47gmqh&apiKey=R_eaee61dc34b6434eaf8adaeb449d52c9&longUrl=" + d_all + "&callback=?",
+ url : "https://api-ssl.bitly.com/v3/shorten?format=txt&login=o_2p0k47gmqh&apiKey=R_eaee61dc34b6434eaf8adaeb449d52c9&longUrl=" + encodeURIComponent(d_all),
  type : "POST",
  contentType : 'application/json; charset=utf-8',
  dataType : "json",
