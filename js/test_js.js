05637ccb9b95a3c6ff167238e412dbc85a17ba87
pingfulin <36759454+pingfulin@users.noreply.github.com>
2019-04-02 11:05:22 +0800

    Update test_js.js

--- js/test_js.js
+++ js/test_js.js
@@ -227,15 +227,15 @@
   }
 
 function b_clicline(){
-var d_o= "%0A" + $("#tw_oth").val();
+var d_o= %0A + $("#tw_oth").val();
 var d_imf= "@" + $("#e_file").val();
 $.post('https://script.google.com/macros/s/AKfycbw34_I1Ruovmoo8PPCd6h6vsxCJrxCRV1WJp6Afo__gogNM-sOe/exec',
     {msg: d_o,
     imagf: d_imf
     },
-    function(e1){
+    function(e1, e2){
         console.log(e1);
-    
+        console.log(e2);    
 });
 }
 	
