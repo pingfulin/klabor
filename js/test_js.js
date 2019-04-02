2d830a6fdad033854f3cc28c5af0776a2cb14c91
pingfulin <36759454+pingfulin@users.noreply.github.com>
2019-04-02 08:24:48 +0800

    Update test_js.js

--- js/test_js.js
+++ js/test_js.js
@@ -233,8 +233,8 @@ $.post('https://script.google.com/macros/s/AKfycbw34_I1Ruovmoo8PPCd6h6vsxCJrxCRV
     {msg: d_o,
     imagf: d_imf
     },
-    function(e){
-        console.log(e);
+    function(e1,e2){
+        console.log(e1,e2);
 });
 }
 	
