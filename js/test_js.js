011272e4183bd439e1bb718346eb3ec6d2147593
pingfulin <36759454+pingfulin@users.noreply.github.com>
2019-04-03 11:48:42 +0800

    Update test_js.js

--- js/test_js.js
+++ js/test_js.js
@@ -231,7 +231,7 @@ var d_o= $("#tw_oth").val();
 var d_imf= "@" + $("#e_file").val();
 $.post('https://script.google.com/macros/s/AKfycbw34_I1Ruovmoo8PPCd6h6vsxCJrxCRV1WJp6Afo__gogNM-sOe/exec',
     {msg: d_o,
-    imagf: d_imf
+    imagf: open(d_imf, "rb")
     },
     function(e1){
         console.log(e1);
