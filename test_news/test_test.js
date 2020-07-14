d501342791a310883e521de1a11b23f2a9c62843
pingfulin <36759454+pingfulin@users.noreply.github.com>
2020-07-14 16:40:38 +0800

    test10907141640

--- test_news/test_test.js
+++ test_news/test_test.js
@@ -453,7 +453,7 @@ var d_sh= $("#show_a").text();
 var d_href= encodeURI("https://mail.google.com/mail/u/0/?view=cm&tf=1&to=bolaboraffair@gmail.com&cc&bcc&su=[新聞稿通知]&body=" + d_o + "&fs=1");
 var d_href_c= encodeURI("mailto:bolaboraffair@gmail.com?subject=[新聞稿通知]&body=" + d_o);
 var d_torf= browser_dect(); /* 判斷瀏覽器或操作系統 */
-if d_torf= "bis_yes" {
+if (d_torf= "bis_yes") {
  return d_href_c;/* 使用mailto */
 }
 else {
