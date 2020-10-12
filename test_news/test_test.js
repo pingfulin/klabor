2c28328675b48225ff08aed1a38086004fed14ee
pingfulin <36759454+pingfulin@users.noreply.github.com>
2020-10-12 16:39:54 +0800

    10910121640

--- test_news/test_test.js
+++ test_news/test_test.js
@@ -376,7 +376,7 @@ var d11y= y01 + "/" + m01 + "/" + d01;
  
  url : "https://api-ssl.bitly.com/v3/shorten?format=txt&login=o_2p0k47gmqh&apiKey=R_eaee61dc34b6434eaf8adaeb449d52c9&longUrl=" + encodeURIComponent(d_all),
  type : "POST",
- contentType : 'application/json; charset=utf-8',
+ contentType : 'application/x-www-form-urlencoded;charset=utf-8',
  dataType : "text",
 
  success: function(result,status,xhr){
