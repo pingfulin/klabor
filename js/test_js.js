922ccf6d50371dacf8f4db5648e0b09acf1c483f
pingfulin <36759454+pingfulin@users.noreply.github.com>
2024-10-11 16:25:55 +0800

    11310111625

--- js/test_js.js
+++ js/test_js.js
@@ -255,7 +255,8 @@ var d11y= y01 + "/" + m01 + "/" + d01 + "_";
    var d_t= $("#kind_s").val().replace(/#|\?|\s/ig,"") + d11y + $("#tw_cnam").val().replace(/#|\?|\s/ig,"");
    //var d_d= $("#tw_p").val() + "%0A" + $("#tw_man").val() + "%0A" +d_o;
    var d_all_1= "https://www.google.com/calendar/event?action=TEMPLATE&text=" + d_t + "&dates=" + d_tall + "&details=" + d_o + "&location=" + d_p;
-   var d_all= {url: d_all_1};
+   var d_all_2= {url: d_all_1};
+   var d_all= JSON.stringify(d_all_2);
 const url= "https://api.reurl.cc/shorten";
 let headers = {
     
@@ -269,7 +270,7 @@ fetch( url,
     method: "POST",
     headers: headers,
     //別忘了把主體参數轉成字串，否則資料會變成[object Object]，它無法被成功儲存在後台
-    body: JSON.stringify(d_all)
+    body: d_all
 })
 .then( response => response.json())
 .then( data => {
